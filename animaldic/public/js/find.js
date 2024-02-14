6d96705cadc848911a7d94195d37489b04714cac
ChumKyungWoo <ckw2434@naver.com>
2024-02-15 08:59:18 +0900

    fish

---
+++ animaldic/public/js/find.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const find_form = document.querySelector();
+
+  document.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.value === "아이디") {
+      return res.render();
+    }
+    if (target.value === "비밀번호") {
+      return res.render();
+    }
+    if (target.value === "다음") {
+      return res.render();
+    }
+    return false;
+  });
+});
